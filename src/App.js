ec6a0b0bbfb416530c4f72254782c85378ea590a
shreyaskinage14 <shreyaskinage14@gmail.com>
2023-03-18 17:35:10 +0530

    face landmark detection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "@tensorflow/tfjs";
+// Register WebGL backend.
+import "@tensorflow/tfjs-backend-webgl";
+import "@mediapipe/face_mesh";
+import Webcam from "react-webcam";
+import { runDetector } from "./utils/detector";
 
+const inputResolution = {
+  width: 1080,
+  height: 900,
+};
+const videoConstraints = {
+  width: inputResolution.width,
+  height: inputResolution.height,
+  facingMode: "user",
+};
 function App() {
+  const canvasRef = useRef(null);
+  const [loaded, setLoaded] = useState(false);
+
+  const handleVideoLoad = (videoNode) => {
+    const video = videoNode.target;
+    if (video.readyState !== 4) return;
+    if (loaded) return;
+    runDetector(video, canvasRef.current);
+    setLoaded(true);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App-header">
+        <Webcam
+          width={inputResolution.width}
+          height={inputResolution.height}
+          style={{ position: "absolute" }}
+          videoConstraints={videoConstraints}
+          onLoadedData={handleVideoLoad}
+        />
+        <canvas
+          ref={canvasRef}
+          width={inputResolution.width}
+          height={inputResolution.height}
+          style={{ position: "absolute" }}
+        />
+        {loaded ? <></> : <header>Loading...</header>}
+      </div>
     </div>
   );
 }
